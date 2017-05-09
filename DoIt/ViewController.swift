2a98eb7dcca2e9f780eb08ed36679e8d83f9c6b1
Nader Balata <nbalata@gmail.com>
2017-05-09 15:25:11 -0400

    Initial Commit

---
+++ DoIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DoIt
+//
+//  Created by Nader Balata on 2017-05-09.
+//  Copyright © 2017 ZMazz Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
